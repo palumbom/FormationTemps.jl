85dc3899efcc85e84507d4a0dbc2d5673b925209
Michael Palumbo <palumbomi95@gmail.com>
2025-10-21 16:24:53 -0400

    rebinning + convolving instrumental code

---
+++ scripts/resolution.jl
@@ -0,0 +1,129 @@
+using Revise
+using FormationTemps; FT = FormationTemps
+using Korg
+using HDF5, Printf
+using CUDA, BenchmarkTools
+using FFTW
+using CSV, DataFrames, Statistics
+using PyPlot, PyCall; mpl = plt.matplotlib
+plt.ioff()
+
+# matplotlib backend
+mpl.use("Qt5Agg")
+mpl.style.use(FT.moddir * "fig.mplstyle")
+inset = pyimport("mpl_toolkits.axes_grid1.inset_locator")
+
+# get fancy fonts
+plt.rc("text", usetex=true)
+plt.rc("text.latex", preamble="\\usepackage{amsmath}
+                               \\usepackage{mathrsfs}")
+
+# python interpolation for matplotlib stuff
+interp1d = pyimport("scipy.interpolate").interp1d
+
+# set colormaps
+img_cmap = "viridis"
+μ_cmap = "autumn"
+
+# alias type 
+AA = AbstractArray
+CA = CuArray
+AF = AbstractFloat
+
+# make plotdir
+plotdir = joinpath(pwd(), "figures")
+!isdir(plotdir) && mkdir(plotdir)
+
+# get the linelist
+linelist = Korg.read_linelist(joinpath(FT.datdir, "Sun_VALD.lin"))
+linelist = [Korg.Line(l, wl=Korg.vacuum_to_air(l.wl)) for l in linelist]
+specs = [string(l.species) for l in linelist]
+
+# cut on species
+linelist = linelist[specs .== "Fe I"]
+
+# get the Fe I 6301 & 6302 lines (just cuz)
+wls = [l.wl for l in linelist] 
+idx1 = findfirst(x -> x * 1e8 .>= 6301, wls)
+idx2 = findfirst(x -> x * 1e8 .>= 6302, wls)
+linelist = vcat([linelist[idx1], linelist[idx2]])
+
+# re-get values
+wls = [l.wl * 1e8 for l in linelist]
+log_gf =  [l.log_gf for l in linelist]
+species =  [l.species for l in linelist]
+E_lower =  [l.E_lower for l in linelist]
+gamma_rad =  [l.gamma_rad for l in linelist]
+gamma_stark =  [l.gamma_stark for l in linelist]
+
+# get some abundances
+A_X = Korg.asplund_2020_solar_abundances
+
+# get the atmosphere
+marcs_atm = FT.get_marcs_atm(5777.0, 4.44, A_X, n_layers=56)
+τ_500 = Korg.get_tau_refs(marcs_atm)
+zs = Korg.get_zs(marcs_atm)
+Ts = Korg.get_temps(marcs_atm)
+ne = Korg.get_electron_number_densities(marcs_atm)
+nd = Korg.get_number_densities(marcs_atm)
+
+# make my atmosphere 
+atm_gpu = FT.AtmosphereGPU(marcs_atm)
+zs = atm_gpu.zs
+Ts = atm_gpu.Ts
+τ5000 = atm_gpu.τs
+
+# make the wavelength grid
+buffer = 0.5
+λs_korg = range(first(wls) - buffer, last(wls) + buffer, step=0.001)
+cont_idx = findfirst(x -> x .>= 6301.3, λs_korg)
+
+# synthesis to get the alphas
+αs = zeros(length(atm_gpu.zs), length(λs_korg))
+αs_cont = zeros(length(atm_gpu.zs), length(λs_korg))
+FT.compute_alpha!(αs, αs_cont, Korg.Wavelengths(λs_korg), linelist, atm_gpu, A_X)
+
+# allocate memory for convolutions
+Nλ = length(λs_korg)
+Natm = size(αs, 1)
+Npad = 100
+cmem = FT.ConvolutionMemory(Nλ, Natm, Npad)
+
+# allocate on device
+gpu_mem = FT.GPUMemory(λs_korg, atm_gpu)
+
+# velocities
+μ_v_rot = CUDA.zeros(Float64, length(zs))
+σ_v_mic = CUDA.zeros(Float64, length(zs)) .+ 1200.0
+
+μ_v_mac = CUDA.zeros(Float64, length(zs)-1)
+σ_v_mac = CUDA.zeros(Float64, length(zs)-1)
+
+cmem_mac = FT.ConvolutionMemory(Nλ, Natm - 1, Npad)
+
+# get the formation temperature for a stationary star
+cfunc_flux_stationary = 2π .* FT.calc_flux_cfunc(αs, atm_gpu, gpu_mem, cmem, σ_v_mic)
+flux_stationary = dropdims(sum(cfunc_flux_stationary, dims=1), dims=1)
+
+cum_cfunc_flux_stationary = cumsum(cfunc_flux_stationary, dims=1)
+cum_cfunc_flux_stationary ./= maximum(cum_cfunc_flux_stationary, dims=1)
+
+form_temp_stationary = zeros(length(λs_korg))
+for i in eachindex(λs_korg)
+    xs = view(cum_cfunc_flux_stationary, :, i)
+    itp = FT.linear_interp(xs, elav(Ts))
+    form_temp_stationary[i] = itp(0.5)
+end
+
+# set rotational and macroturbulence 
+vsini = 2100.0
+ζ_rt = 1400.0
+
+# create resolution grid 
+Δλ_grid = range(0.001, 0.1001, length=5)
+R_grid = [10_000.0, 25_000.0, 50_000.0, 75_000.0, 100_000.0]
+
+for i in eachindex(Δλ_grid)
+
+
+end
--- src/instrumental.jl
+++ src/instrumental.jl
@@ -1,4 +1,152 @@
-function convolve_instrumental_profile()
+# follows from implementation at https://github.com/ACCarnall/SpectRes/blob/master/spectres/spectral_resampling.py
+function get_bin_edges(arr::AA{T,1}) where T<:AF
+    edges = zeros(length(arr)+1)
+    edges[1] = arr[1] - 0.5 * (arr[2] - arr[1])
+    edges[end] = arr[end] + 0.5 * (arr[end] - arr[end-1])
+    edges[2:end-1] = 0.5 .* (arr[2:end] .+ arr[1:end-1])
+    widths = diff(edges)
+    return edges, widths
+end
 
-    return nothing
+# follows from implementation at https://github.com/ACCarnall/SpectRes/blob/master/spectres/spectral_resampling.py
+# TODO algorithm might cause shift based on input wavelength grid?
+# might cause issue if wavelengths shift across
+function rebin_spectrum(xs_old::AA{T,1}, ys_old::AA{T,1}, xs_new::AA{T,1}) where T<:AF
+    @assert issorted(xs_old)
+    @assert issorted(xs_new)
+
+    # get edges of bins
+    old_edges, old_widths = get_bin_edges(xs_old)
+    new_edges, new_widths = get_bin_edges(xs_new)
+
+    # allocate memory for output array
+    ys_new = zeros(length(xs_new))
+
+    # loop over new bins
+    start = 0
+    stop = 0
+    for i in eachindex(xs_new)
+        # boundary conditions
+        if new_edges[i] < first(old_edges)
+            ys_new[i] = first(ys_old)
+            continue
+        elseif new_edges[i+1] > last(old_edges)
+            ys_new[i] = last(ys_old)
+            continue
+        end
+
+        while old_edges[start+1] <= new_edges[i]
+            start += 1
+        end
+
+        while old_edges[stop+1] < new_edges[i+1]
+            stop += 1
+        end
+
+        if start == stop
+            ys_new[i] = ys_old[start]
+        else
+            start_factor = ((old_edges[start+1] - new_edges[i]) / (old_edges[start+1] - old_edges[start]))
+            stop_factor = ((new_edges[i+1] - old_edges[stop]) / (old_edges[stop+1] - old_edges[stop]))
+
+            old_widths[start] *= start_factor
+            old_widths[stop] *= stop_factor
+
+            f_widths = old_widths[start:stop] .* ys_old[start:stop]
+            ys_new[i] = sum(f_widths)
+            ys_new[i] /= sum(old_widths[start:stop])
+
+            old_widths[start] /= start_factor
+            old_widths[stop] /= stop_factor
+        end
+    end
+    return ys_new
+end
+
+# follows from implementation at https://github.com/ACCarnall/SpectRes/blob/master/spectres/spectral_resampling.py
+function rebin_spectrum(xs_old::AA{T,1}, ys_old::AA{T,1}, σs_old::AA{T,1}, xs_new::AA{T,1}, ) where T<:AF
+    @assert length(σs_old) == length(ys_old)
+    @assert issorted(xs_old)
+    @assert issorted(xs_new)
+
+    # get edges of bins
+    old_edges, old_widths = get_bin_edges(xs_old)
+    new_edges, new_widths = get_bin_edges(xs_new)
+
+    # allocate memory for output arrays
+    ys_new = zeros(length(xs_new))
+    σs_new = zeros(length(xs_new))
+
+    # loop over new bins
+    start = 0
+    stop = 0
+    for i in eachindex(xs_new)
+        # boundary conditions
+        if new_edges[i] < first(old_edges)
+            ys_new[i] = first(ys_old)
+            σs_new[i] = first(σs_old)
+            continue
+        elseif new_edges[i+1] > last(old_edges)
+            ys_new[i] = last(ys_old)
+            σs_new[i] = last(σs_old)
+            continue
+        end
+
+        while old_edges[start+1] <= new_edges[i]
+            start += 1
+        end
+
+        while old_edges[stop+1] < new_edges[i+1]
+            stop += 1
+        end
+
+        if start == stop
+            ys_new[i] = ys_old[start]
+            σs_new[i] = σs_old[start]
+        else
+            start_factor = ((old_edges[start+1] - new_edges[i]) / (old_edges[start+1] - old_edges[start]))
+            stop_factor = ((new_edges[i+1] - old_edges[stop]) / (old_edges[stop+1] - old_edges[stop]))
+
+            old_widths[start] *= start_factor
+            old_widths[stop] *= stop_factor
+
+            f_widths = old_widths[start:stop] .* ys_old[start:stop]
+            ys_new[i] = sum(f_widths)
+            ys_new[i] /= sum(old_widths[start:stop])
+
+            e_wid = old_widths[start:stop] .* σs_old[start:stop]
+            σs_new[i] = sqrt(sum(e_wid.^2.0))
+            σs_new[i] /= sum(old_widths[start:stop])
+
+            old_widths[start] /= start_factor
+            old_widths[stop] /= stop_factor
+        end
+    end
+    return ys_new, σs_new
+end
+
+function convolve_instrument_gauss(xs::AA{T,1}, ys::AA{T,1}; new_res::T=1.17e5,
+                                   oversampling::T=2.0) where T<:AbstractFloat
+    # get kernel
+    σ(x) = x / new_res / (2.0 * sqrt(2 * log(2)))
+    g(x, n) = (one(T)/(σ(x) * sqrt(2.0 * π))) * exp(-0.5 * ((x - n)/σ(x))^2)
+
+    # offset the kernel by the velocity
+    λ0 = mean(xs)
+    λc = (μ_v / c_ms) * λ0 + λ0
+
+    # sample and normalize the kernel
+    kernel = g.(xs, λc)
+    kernel ./= sum(kernel)
+
+    # convolve it
+    convolved = imfilter(ys, reflect(centered(kernel)), Pad(:replicate), ImageFiltering.FFT())
+
+    # get wavelength grid at lower resolution
+    Δlnλ = 1.0 / new_res
+    lnλs = range(log(first(xs)), log(last(xs)), step=Δlnλ/oversampling)
+    xs_out = exp.(lnλs)
+
+    # return rebinned spectrum
+    return rebin_spectrum(xs, convolved, xs_out)
 end
